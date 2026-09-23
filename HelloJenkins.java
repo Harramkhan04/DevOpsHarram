cececb308ce7eaa114a63e6add0666eac6e8e582
Harramkhan04 <harrammansoorkhan11@gmail.com>
2026-09-23 01:22:28 -0700

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -2,5 +2,6 @@ public class HelloJenkins {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins CI/CD pipeline!");
         System.out.println("Build triggered successfully.");
+        System.out.println("Second test run.");
     }
 }
